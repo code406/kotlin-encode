653da5be5b9312dc144e3cf9a25318a8123ef745
CodeNotAcceptable <davidpm99@gmail.com>
2021-02-12 20:57:22 +0100

    testing git from idea

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,9 @@
 fun main() {
-    var num = 1
-    var resultado: Int
-    resultado = if (num >= 2) 1
-    else if (num < 7) 2
-    else 3
-    println(resultado)
+    val num = 1
+    val resultado = when {
+        num >= 2 -> 1
+        num < 7 -> 2
+        else -> 3
+    }
+    print(resultado)
 }
